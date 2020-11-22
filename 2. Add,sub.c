68e14a560ec8d359a7f47b3f03509e2b164d6876
Md. Muzahidul Islam Khandakar <72061868+muzahid626@users.noreply.github.com>
2020-11-22 18:38:33 +0600

    Add files via upload

---
+++ 2. Add,sub.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b,x;
+    scanf("%d%d",&a,&b);
+    x=a+b;
+
+    printf("Add = %d\n",x);
+    x=a-b;
+    printf("Sub = %d\n",x);
+    x=a*b;
+    printf("Multiple = %d\n",x);
+    x=a/b;
+    printf("Division = %d\n",x);
+    return 0;
+}
+
